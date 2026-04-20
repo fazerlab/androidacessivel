71c119e02d8b3735027f3f6ce2e1b53cc1b09376
fazerlab <fazerlab@gmail.com>
2026-04-20 10:34:03 -0300

    fix real apk build

--- app/src/main/java/com/example/udpaccess/MainActivity.java
+++ app/src/main/java/com/example/udpaccess/MainActivity.java
@@ -8,8 +8,9 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Ative o serviço de acessibilidade.");
-        setContentView(tv);
+        //TextView tv = new TextView(this);
+        //tv.setText("Ative o serviço de acessibilidade.");
+        //setContentView(tv);
+		setContentView(R.layout.activity_main);
     }
 }
